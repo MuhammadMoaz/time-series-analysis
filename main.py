b5df4cde7837f8df5ce61f9ce9c6dd8c0d79f823
Muhammad Moaz <muhammadmoaz2020@outlook.com>
2025-08-06 18:48:40 +1000

    fetched, downloaded, and saved data for top 3 banks to csv's

---
+++ main.py
@@ -0,0 +1,23 @@
+import yfinance as yf
+
+# Fetching, downloading, and saving CBA.AX, WBC.AX, and NAB.AX data
+# CBA.AX
+cba_ticker = "CBA.AX"
+cba_data = yf.download(cba_ticker, period="max")
+print(cba_data.head())
+print(cba_data.shape)
+cba_data.to_csv("CBA_Market_Data.csv")
+
+# WBC.AX
+wbc_ticker = "WBC.AX"
+wbc_data = yf.download(wbc_ticker, period="max")
+print(wbc_data.head())
+print(wbc_data.shape)
+wbc_data.to_csv("WBC_Market_Data.csv")
+
+# NAB.AX
+nab_ticker = "NAB.AX"
+nab_data = yf.download(nab_ticker, period="max")
+print(nab_data.head())
+print(nab_data.shape)
+nab_data.to_csv("NAB_Market_Data.csv")
