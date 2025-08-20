58a6ba63bc27b7d6bfe0f6c95f4657743b11c22d
oli <olidavey88@icloud.com>
2025-08-21 00:19:12 +1000

    changed main func

--- main.py
+++ main.py
@@ -13,32 +13,19 @@ def fetch_data(ticker_symbol):
 def preprocessing(df):
     print("preprocessing")
 
-def perform_eda(df):
-    # print(df.head())
-    # print(df.tail())
-    # print(df.shape)
-    # print(df.describe())
+def perform_eda():
     EDA.main()
 
 def perform_pda(df):
     print("pda")
 
 def main():
-<<<<<<< HEAD
     # Financials
-=======
     # Banking Stocks
->>>>>>> 44e69293fc162daf522c247b7013b389c970fdf1
     cba_data = fetch_data("CBA.AX")
-    perform_eda(cba_data)
-
     wbc_data = fetch_data("WBC.AX")
-    perform_eda(wbc_data)
-
     nab_data = fetch_data("NAB.AX")
-    perform_eda(nab_data)
 
-<<<<<<< HEAD
     # Materials
     bhp_data = fetch_data("BHP.AX")
     amc_data = fetch_data("AMC.AX")
@@ -49,19 +36,5 @@ def main():
     rmd_data = fetch_data("RMD.AX")
     pme_data = fetch_data("PME.AX")
 
-=======
-    # Materials Stocks
-    bhp_data = fetch_data("BHP.AX")
-    perform_eda(bhp_data)
-
-    nem_data = fetch_data("NEM.AX")
-    perform_eda(nem_data)
-
-    fmg_data = fetch_data("FMG.AX")
-    perform_eda(fmg_data)
-
-    # Pharma Stocks
-    
-    
->>>>>>> 44e69293fc162daf522c247b7013b389c970fdf1
+    perform_eda()
 main()
