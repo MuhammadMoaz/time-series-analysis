32780f29d23d2858a3bae3cea77d6ee7a5893e54
Oli Davey <olidavey88@gmail.com>
2025-08-25 20:11:24 +1000

    added pre processing file

--- main.py
+++ main.py
@@ -3,17 +3,6 @@ import pandas as pd
 import matplotlib
 import EDA
 
-def fetch_data(ticker_symbol):
-    # 261 does withot weekends in a year
-    # 261 * 24 years is 6000 or something
-    data = yf.download(ticker_symbol, start="2001-01-01")
-    filename = ticker_symbol + "_data.csv"
-    data.to_csv(f"datasets/{filename}")
-
-    # filename = ticker_symbol + "_data.csv"
-    data.to_csv(f"datasets/temp")
-    return data
-
 def preprocessing(df):
     print("preprocessing")
 
@@ -24,21 +13,6 @@ def perform_pda(df):
     print("pda")
 
 def main():
-    # Financials
-    cba_data = fetch_data("CBA.AX")
-    wbc_data = fetch_data("WBC.AX")
-    nab_data = fetch_data("NAB.AX")
-
-    # Materials
-    bhp_data = fetch_data("BHP.AX")
-    amc_data = fetch_data("AMC.AX")
-    rio_data = fetch_data("RIO.AX")
-
-    # Healthcare
-    csl_data = fetch_data("CSL.AX")
-    rmd_data = fetch_data("RMD.AX")
-    pme_data = fetch_data("PME.AX")
-
     perform_eda()
     
     
---
+++ preprocessing.py
@@ -0,0 +1,25 @@
+import yfinance as yf
+import pandas as pd
+
+def fetch_data(ticker_symbol):
+    # 261 does withot weekends in a year
+    # 261 * 24 years is 6000 or something
+    data = yf.download(ticker_symbol, start="2001-01-01")
+    filename = ticker_symbol + "_data.csv"
+    data.to_csv(f"datasets/{filename}")
+
+def main():
+    # Banks
+    fetch_data("CBA.AX")
+    fetch_data("WBC.AX")
+    fetch_data("NAB.AX")
+
+    # Materials
+    fetch_data("BHP.AX")
+    fetch_data("AMC.AX")
+    fetch_data("RIO.AX")
+
+    # Healthcare
+    fetch_data("CSL.AX")
+    fetch_data("RMD.AX")
+    fetch_data("PME.AX")
