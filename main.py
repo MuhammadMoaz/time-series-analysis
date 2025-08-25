146acb357850981899e10a0fac32f970a20f0c93
Muhammad Moaz <muhammadmoaz2020@outlook.com>
2025-08-25 17:59:12 +1000

    merging commit

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import EDA
 
 def fetch_data(ticker_symbol):
     data = yf.download(ticker_symbol, period="max")
-    filename = ticker_symbol + "_data.csv"
-    data.to_csv(f"datasets/{filename}")
+    # filename = ticker_symbol + "_data.csv"
+    data.to_csv(f"datasets/temp")
     return data
 
 def preprocessing(df):
@@ -24,24 +24,27 @@ def perform_pda(df):
 
 def main():
     # Banking Stocks
-    cba_data = fetch_data("CBA.AX")
-    perform_eda(cba_data)
+    banking_tickers = ["CBA.AX", "WBC.AX", "NAB.AX"]
+    banking_data = fetch_data(banking_tickers)
 
-    wbc_data = fetch_data("WBC.AX")
-    perform_eda(wbc_data)
+    # cba_data = fetch_data("CBA.AX")
+    # perform_eda(cba_data)
 
-    nab_data = fetch_data("NAB.AX")
-    perform_eda(nab_data)
+    # wbc_data = fetch_data("WBC.AX")
+    # perform_eda(wbc_data)
+
+    # nab_data = fetch_data("NAB.AX")
+    # perform_eda(nab_data)
 
     # Materials Stocks
-    bhp_data = fetch_data("BHP.AX")
-    perform_eda(bhp_data)
+    # bhp_data = fetch_data("BHP.AX")
+    # perform_eda(bhp_data)
 
-    nem_data = fetch_data("NEM.AX")
-    perform_eda(nem_data)
+    # nem_data = fetch_data("NEM.AX")
+    # perform_eda(nem_data)
 
-    fmg_data = fetch_data("FMG.AX")
-    perform_eda(fmg_data)
+    # fmg_data = fetch_data("FMG.AX")
+    # perform_eda(fmg_data)
 
     # Pharma Stocks
     
