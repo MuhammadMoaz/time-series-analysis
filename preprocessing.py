4bce142d895e1ef04b193d710aafaf4c85ffd595
oli <olidavey88@icloud.com>
2025-09-01 18:37:03 +1000

    og

--- preprocessing.py
+++ preprocessing.py
@@ -5,25 +5,23 @@ def fetch_data(ticker_symbol):
     # 261 does withot weekends in a year
     # 261 * 24 years is 6000 or something
     data = yf.download(ticker_symbol, start="2001-01-01")
-    # data = data.reset_index(['Date'])
-    # filename = ticker_symbol + "_data.csv"
-    data.to_csv(f"datasets/temp_data.csv")
+    filename = ticker_symbol + "_data.csv"
+    data.to_csv(f"datasets/{filename}")
 
 def main():
     # Banks
-    # fetch_data("CBA.AX")
-    # fetch_data("WBC.AX")
-    # fetch_data("NAB.AX")
+    fetch_data("CBA.AX")
+    fetch_data("WBC.AX")
+    fetch_data("NAB.AX")
 
-    # # Materials
-    # fetch_data("BHP.AX")
-    # fetch_data("AMC.AX")
-    # fetch_data("RIO.AX")
+    # Materials
+    fetch_data("BHP.AX")
+    fetch_data("AMC.AX")
+    fetch_data("RIO.AX")
 
-    # # Healthcare
-    # fetch_data("CSL.AX")
-    # fetch_data("RMD.AX")
-    # fetch_data("PME.AX")
+    # Healthcare
+    fetch_data("CSL.AX")
+    fetch_data("RMD.AX")
+    fetch_data("PME.AX")
 
-    tks = ['CBA.AX', 'WBC.AX', 'NAB.AX']
-    fetch_data(tks)
+main()
