1c48a76c581e7435fd1df5d4c7c284075ae3f87a
oli <olidavey88@icloud.com>
2025-09-01 18:44:07 +1000

    fixed csv multi level indexing issue

--- preprocessing.py
+++ preprocessing.py
@@ -4,8 +4,9 @@ import pandas as pd
 def fetch_data(ticker_symbol):
     # 261 does withot weekends in a year
     # 261 * 24 years is 6000 or something
-    data = yf.download(ticker_symbol, start="2001-01-01")
+    data = yf.download(ticker_symbol, start="2001-01-01", multi_level_index=False)
     filename = ticker_symbol + "_data.csv"
+    # data = data.reset_index['Date']
     data.to_csv(f"datasets/{filename}")
 
 def main():
