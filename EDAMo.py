f4e7fd9749c1bebf8832742a5dff9ced76dffc13
Oli Davey <olidavey88@gmail.com>
2025-09-02 14:26:46 +1000

    yes

---
+++ EDAMo.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from os import makedirs
+from pathlib import Path
+
+'''
+This File takes csv files in as input and performs an EDA on them, 
+it then outputs data into folders with appropriate titling, these 
+folders will contain graphs of different types to visualise data
+'''
+# function calls all EDA making functions
+def edaMaker():
+    print("edaMaker")
+
+# output import check as txt file, and graphs as pngs, 
+# store them in folders related by ticker codes
+# eda function 1
+# eda function 2
+# eda function 3
+# eda function 4
+
+def importCheckFile(data, fn):
+    strippedName = fn.replace("datasets\\","").rstrip("AX_data.csv")
+    dirName = f"EDAOutput/EDA_{strippedName}"
+    makedirs(dirName, exist_ok=True)
+
+    with open(f"EDAOutput/EDA_{strippedName}/{strippedName}.txt", 'w+') as f:
+        f.write(f"{strippedName} EDA Output:\n")
+        f.write(f"Variables: {data.columns.tolist()}\n")
+        f.write(f"Head: \n{data.head()}\n")
+        f.write(f"Tail: \n{data.tail()}\n")
+        f.write(f"Shape: \n{data.shape}\n")
+        f.write(f"{data.info(verbose=True)}\n")
+        f.write(f"Empty Cells: \n{data.isnull().sum()}")
+
+# function opens csv
+def fileOpener(fn):
+    data = pd.read_csv(fn)
+    importCheckFile(data, fn)
+    
+def main():
+    pathlist = Path("datasets").rglob("*.csv")
+    for p in pathlist:
+        strPath = str(p)
+        fileOpener(strPath)
