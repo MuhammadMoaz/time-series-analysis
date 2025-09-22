7e79262d3798e26f09602f31c9e6a31bf0fbf14b
Oli Davey <olidavey88@gmail.com>
2025-09-22 19:53:18 +1000

    Added new line graph subplots

--- EDA.py
+++ EDA.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-from os import makedirs
+import os
 from pathlib import Path
 
 '''
@@ -10,51 +10,22 @@ This File takes csv files in as input and performs an EDA on them,
 it then outputs data into folders with appropriate titling, these 
 folders will contain graphs of different types to visualise data
 '''
-# function calls all EDA making functions
-def edaMaker():
-    print("edaMaker")
 
-# output import check as txt file, and graphs as pngs, 
-# store them in folders related by ticker codes
-# eda function 1
-# eda function 2
-# eda function 3
-# eda function 4
-
-def nameStripper(file_name):
+def get_ticker(file_name):
     strName = file_name.replace("datasets\\","")
     NameParts = strName.split('.')
     return NameParts[0]
 
-# def closeVsTime(data, file_name):
-#     strippedName = nameStripper(fn)
-#     dirName = f"EDAOutput/EDA_{strippedName}"
-#     file_path = f"{dirName}/{strippedName}_corr.png"
+def create_output_folder(file_name, ticker):
+    dirName = f"EDAOutput/EDA_{ticker}"
+    os.makedirs(dirName, exist_ok=True)
 
-#     columns = data.columns.tolist()
-#     # columns.remove('Date')
-#     col_data = data[columns]
-#     corr_matrix = col_data.corr()
-#     sns.heatmap(corr_matrix, cmap="YlGnBu", annot=True)
-#     plt.savefig(file_path)
+def summarise_data(data, file_path):
+    ticker = get_ticker(file_path)
+    data = pd.read_csv(file_path)
 
-def genCorrMatrix(data, sname):
-    dirName = f"EDAOutput/EDA_{sname}"
-    file_path = f"{dirName}/{sname}_corr.png"
-
-    columns = data.columns.tolist()
-    # columns.remove('Date')
-    col_data = data[columns]
-    corr_matrix = col_data.corr()
-    sns.heatmap(corr_matrix, cmap="YlGnBu", annot=True)
-    plt.savefig(file_path)
-
-def importCheckFile(data, file_name):
-    dirName = f"EDAOutput/EDA_{file_name}"
-    makedirs(dirName, exist_ok=True)
-
-    with open(f"EDAOutput/EDA_{file_name}/{file_name}.txt", 'w+') as f:
-        f.write(f"{file_name} EDA Output:\n")
+    with open(f"EDAOutput/EDA_{ticker}/{ticker}.txt", 'w+') as f:
+        f.write(f"{ticker} EDA Output:\n")
         f.write(f"Variables: {data.columns.tolist()}\n")
         f.write(f"Head: \n{data.head()}\n")
         f.write(f"Tail: \n{data.tail()}\n")
@@ -62,17 +33,94 @@ def importCheckFile(data, file_name):
         f.write(f"{data.info(verbose=True)}\n")
         f.write(f"Empty Cells: \n{data.isnull().sum()}")
 
-# function opens csv
-def fileOpener(file_name):
-    data = pd.read_csv(file_name)
-    sname = nameStripper(file_name)
-    importCheckFile(data, sname)
-    genCorrMatrix(data, sname)
+# Visualisation 1 - Histogram
+def genHistogram(data, ticker):
+    dirName = f"EDAOutput/EDA_{ticker}"
     
-def main():
-    pathlist = Path("datasets").rglob("*.csv")
-    for p in pathlist:
-        strPath = str(p)
-        fileOpener(strPath)
+    columns = data.columns.tolist()
+    columns.remove("Date")
+    
+    for var in columns:
+        file_path = f"{dirName}/{ticker}_{var}_Hist.png"
+        
+        if os.path.exists(file_path):
+            continue
+        else:
+            plt.hist(data[var], color='purple')
+            plt.title(f'Distribution of {var} Price')
+            plt.xlabel(f'{var} Price')
+            plt.ylabel('Count')
+            plt.savefig(file_path)
+            plt.clf()
 
+# Visualisation 2 - Correlation Matrix
+def genCorrMatrix(data, ticker):
+    dirName = f"EDAOutput/EDA_{ticker}"
+    file_path = f"{dirName}/{ticker}_corr.png"
+
+    columns = data.columns.tolist()
+    columns.remove('Date')
+    col_data = data[columns]
+    corr_matrix = col_data.corr()
+    sns.heatmap(corr_matrix, cmap="YlGnBu", annot=True)
+    plt.savefig(file_path)
+    plt.clf()
+
+# Visualisation 3 - Line Graph
+def genLineGraph(data, ticker):
+    dirName = f"EDAOutput/EDA_{ticker}"
+
+    file_path = f"{dirName}/{ticker}_Line.png"
+
+    x_val = data['Date']
+    y_val = data['Close']
+
+    plt.plot(x_val, y_val)
+
+    plt.xlabel("X-Axis Label")
+    plt.ylabel("Y-Axis Label")
+    plt.title("Line Graph")
+
+    plt.savefig(file_path)
+    plt.clf()
+
+
+# Visualisation X - gen line subplots
+def genLineSub():
+    datasets = Path("datasets").rglob("*.csv")
+    plt.figure(figsize=(20,10))
+
+    for i, data in enumerate(datasets, 1):
+        file_name = str(data)
+        ticker = get_ticker(file_name)
+        df = pd.read_csv(data)
+
+        plt.subplot(3,3,i)
+        plt.plot(df["Date"], df["Close"])
+        df["Close"].plot()
+        # plt.ylabel("Close Price")
+        # plt.xlabel(None)
+        plt.title(f"{ticker} Closing Price")
+        
+        
+    plt.tight_layout()
+    plt.show()
+
+
+def main():
+    datasets = Path("datasets").rglob("*.csv")
+
+    for data in datasets:
+        file_name = str(data)
+        df = pd.read_csv(data)
+
+        ticker = get_ticker(file_name)
+        create_output_folder(file_name, ticker)
+        summarise_data(df, file_name)
+
+        # genHistogram(df, ticker)
+        # genCorrMatrix(df, ticker)
+        # genLineGraph(df, ticker)
+
+    genLineSub()
 main()
