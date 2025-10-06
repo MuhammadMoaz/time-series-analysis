93d44683f3ea824485823a7b678cbedeb1100e41
oli <olidavey88@icloud.com>
2025-10-06 23:46:43 +1100

    implementing AIC tool

--- arima.py
+++ arima.py
@@ -10,20 +10,62 @@ from sklearn.model_selection import train_test_split
 import seaborn as sns
 from pathlib import Path
 from sklearn.metrics import mean_squared_error
+import os
+
+
+def create_output_folder(ticker):
+    dirName = f"ARIMAOutput/ARIMA_{ticker}"
+    os.makedirs(dirName, exist_ok=True)
 
 def get_ticker(file_name):
     strName = file_name.replace("datasets\\","")
     NameParts = strName.split('.')
     return NameParts[0]
 
+def AICHelper(df, ticker):
+    df.dropna()
+
+    train_size = int(len(df)*0.8) #80 / 20 train test split
+    # test train, dataframes 
+    train_set = df.iloc[:train_size]
+
+    lowest_AIC = 0
+    best_p = 0
+    best_q = 0
+
+    for p in range(6):
+        for q in range(6):
+            try:
+                model = ARIMA(train_set["Close"], order=(p, 1, q))
+                result = model.fit()
+
+                if p == 0 and q == 0:
+                    lowest_AIC = result.aic
+                elif result.aic < lowest_AIC:
+                    lowest_AIC = result.aic
+                    best_p = p
+                    best_q = q
+
+            except:
+                print ("No AIC value")
+
+    print (ticker, best_p, 1, best_q, ": ", result.aic)
+        
+
 
 def CustomARIMA(df, ticker, p, d, q):
-    df.drop(index=0)
-    X_train, X_test, y_train, y_test = train_test_split(df["Date"], df["Close"], train_size=len(df)-30, random_state=42)
+    df.dropna()
 
-    model = ARIMA(y_train, order=(p,d,q))
+    train_size = int(len(df)*0.8) #80 / 20 train test split
+    # test train, dataframes 
+    train_set = df.iloc[:train_size]
+    test_set = df.iloc[train_size:]
+
+    model = ARIMA(train_set["Close"], order=(p,d,q))
     res = model.fit()
     print(res.summary())
+    res.plot_diagnostics(figsize=(20,15))
+    plt.show()
 
 
 
@@ -31,31 +73,37 @@ def main():
     datasets = Path("datasets").rglob("*.csv")
     dataset_list = []
     ticker_list = []
+
     for data in datasets:
-        dataset_list.append(str(data))
-        ticker_list.append(get_ticker(str(data)))
+        filename = str(data)
+        ticker = get_ticker(str(data))
+        create_output_folder(ticker)
+
+        dataset_list.append(filename)
+        ticker_list.append(ticker)
+
+        df = pd.read_csv(filename)
+        # AICHelper(df, ticker)
 
 
-    df = pd.read_csv(dataset_list[0])
-
-    # ARIMA 1
-    CustomARIMA(pd.read_csv(dataset_list[0]),ticker_list[0],1,1,0)
-    # ARIMA 2
-    CustomARIMA(pd.read_csv(dataset_list[1]),ticker_list[1],1,1,0)
-    # ARIMA 3
-    CustomARIMA(pd.read_csv(dataset_list[2]),ticker_list[2],1,1,0)
-    # ARIMA 4
-    CustomARIMA(pd.read_csv(dataset_list[3]),ticker_list[3],1,1,0)
-    # ARIMA 5
-    CustomARIMA(pd.read_csv(dataset_list[4]),ticker_list[4],1,1,0)
-    # ARIMA 6
-    CustomARIMA(pd.read_csv(dataset_list[5]),ticker_list[5],1,1,0)
-    # ARIMA 7
-    CustomARIMA(pd.read_csv(dataset_list[6]),ticker_list[6],1,1,0)
-    # ARIMA 8
-    CustomARIMA(pd.read_csv(dataset_list[7]),ticker_list[7],1,1,0)   
-    # ARIMA 9
-    CustomARIMA(pd.read_csv(dataset_list[8]),ticker_list[8],1,1,0)
+    # # ARIMA 1 AMC
+    CustomARIMA(pd.read_csv(dataset_list[0]),ticker_list[0],5,1,5)
+    # # ARIMA 2 BHP
+    # CustomARIMA(pd.read_csv(dataset_list[1]),ticker_list[1],1,1,0)
+    # # ARIMA 3 CBA
+    # CustomARIMA(pd.read_csv(dataset_list[2]),ticker_list[2],1,1,0)
+    # # ARIMA 4 CSL
+    # CustomARIMA(pd.read_csv(dataset_list[3]),ticker_list[3],1,1,0)
+    # # ARIMA 5 NAB
+    # CustomARIMA(pd.read_csv(dataset_list[4]),ticker_list[4],1,1,0)
+    # # ARIMA 6 PME
+    # CustomARIMA(pd.read_csv(dataset_list[5]),ticker_list[5],1,1,0)
+    # # ARIMA 7 RIO
+    # CustomARIMA(pd.read_csv(dataset_list[6]),ticker_list[6],1,1,0)
+    # # ARIMA 8 RMD
+    # CustomARIMA(pd.read_csv(dataset_list[7]),ticker_list[7],1,1,0)   
+    # # ARIMA 9 WBC
+    # CustomARIMA(pd.read_csv(dataset_list[8]),ticker_list[8],1,1,0)
 
 
 main()
