d617d3a3457b5909d44c651fedd920f4b1f5f56e
Oli Davey <olidavey88@gmail.com>
2025-10-14 14:49:04 +1100

    added ui menu

--- main.py
+++ main.py
@@ -1,23 +1,33 @@
-import yfinance as yf
-import pandas as pd
-import matplotlib
-import preprocessing
-import EDA
-import arima
-import lstm
-import random_forest
-import mars
 
-def preprocessing(df):
-    print("preprocessing yipee")
 
-def perform_eda():
-    EDA.main()
+# def preprocessing(df):
+#     print("preprocessing yipee")
 
-def perform_pda(df):
-    print("pda")
+# def perform_eda():
+#     EDA.main()
+
+# def perform_pda(df):
+#     print("pda")
+
+def uiChooser():
+    eda_choice = 'y'
+    pda_choice = 0
+
+    print("--------------------------------------------")
+    print("Time Series Forcasting EDA and PDA tool")
+    print("Enter corrosponding keys to choose settings:")
+    print("EDA: (y)es (n)o")
+    eda_choice = input("Input: ").lower()
+    print("Model choice:")
+    print("ARIMA: 1")
+    print("LSTM: 2")
+    print("MARS: 3")
+    print("Random Forest: 4")
+    print("All: 5 (May take a while)")
+    pda_choice = int(input("Input: "))
+    print("--------------------------------------------")
 
 def main():
-    perform_eda()
+    uiChooser()
     
 main()
